2c9c78f84767b91f94c9c7f534e8a028f00d5113
ice <meltedise@gmail.com>
2012-05-12 11:22:38 +0900

    created new project by following command:
    rails new kanoko -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanoko::Application.config.secret_token = '47d246bcc40cebbe1167346a52838c0ffb23e8bb7ea74c474702f457a419c148ca9e5904a99f92bd1465badb02f529b93a1b42ce44df6bd60e6beed671d8af90'
