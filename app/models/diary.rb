ca32b62040bc3db2ce68831a2c17414c593b20b4
ice <meltedise@gmail.com>
2012-05-13 02:11:36 +0900

    added Diary#baton_pass method and its tests.

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -3,4 +3,15 @@ class Diary < ActiveRecord::Base
   has_many :members
   has_many :users, through: :members
   has_many :articles
+
+  def next_baton_owner
+    sorted_members = members.sort { |m, n| m.sort <=> n.sort }
+    i = sorted_members.index { |m| m.user_id == baton }
+    (sorted_members[i+1].user rescue nil) || sorted_members.first.user
+  end
+
+  def baton_pass!
+    self.baton = next_baton_owner.id
+    save!
+  end
 end
