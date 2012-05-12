0e9efc84b7f4ea70375faa7893fde259fc96c997
satomicchy <satomi_t_sora@yahoo.co.jp>
2012-05-12 21:48:27 +0900

    change redirect_to root to diaries_path after loged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_path, :notice => "Logged in"
+    redirect_to diaries_path, :notice => "Logged in"
   end
 
   def destroy
