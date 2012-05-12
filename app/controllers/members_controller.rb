ab73c38a20182c769482cac5e4da18f6ad805d44
yalab <rudeboyjet@gmail.com>
2012-05-12 23:50:51 +0900

    Implement members#create most small.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,17 @@
+# -*- coding: utf-8 -*-
 class MembersController < ApplicationController
+  before_filter :login_required
+
   def create
+    @diary = current_user.diaries.find(params[:diary_id])
+    member = @diary.members.new
+    provider = params[:member][:provider]
+    name = params[:member][:account_name]
+    account = Account.find_by_provider_and_name(provider, name) || Account.create(provider: provider, name: name)
+    member.user = account.create_user
+    member.confirm_hash = Digest::SHA1.hexdigest(Time.now.to_f.to_s)
+    member.save
+    redirect_to diaries_path, :notice => name + "を招待しました"
   end
 
   def confirm
--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe MembersController do
+  before do
+    @user = create(:user)
+    @diary = @user.diaries.first
+    session[:user_id] = @user.id
+  end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+  def valid_params
+    {diary_id: @diary.to_param, member: {account_name: "yalab", provider: "twitter"}}
+  end
+
+  describe "invite not a user" do
+    subject { expect { post :create, valid_params }.to }
+    it "create @diary.members" do
+      change(@diary.members, :count).by(1)
+    end
+
+    it "create Account" do
+      change(Account, :count).by(1)
     end
   end
 
@@ -15,5 +28,4 @@ describe MembersController do
       response.should be_success
     end
   end
-
 end
