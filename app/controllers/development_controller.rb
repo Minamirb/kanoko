7ae378d0a569b59147acfb2e37990eaff59e0c66
ice <meltedise@gmail.com>
2012-05-12 21:47:08 +0900

    fixed development/login.

--- app/controllers/development_controller.rb
+++ app/controllers/development_controller.rb
@@ -18,15 +18,17 @@ class DevelopmentController < ApplicationController
       return
     end
 
+    @user = nil
+
     if params[:name]
-      user = User.find_by_name(params[:name])
+      @user = User.find_by_name(params[:name])
     end
 
-    unless user
+    unless @user
       number = User.count + 1
-      user = FactoryGirl.create(:user, name: "user%04d" % number)
+      @user = FactoryGirl.create(:user, name: "user%04d" % number)
       attrs = {
-        user_id:  user.id,
+        user_id:  @user.id,
         provider: params[:provider] || 'development',
         uid:      params[:uid]      || "%04d" % number,
         name:     params[:name]     || "dev%04d" % number,
@@ -34,7 +36,7 @@ class DevelopmentController < ApplicationController
       account = FactoryGirl.create(:account, attrs)
     end
 
-    session[:user_id] = user.id
+    session[:user_id] = @user.id
     redirect_to root_url
   end
 end
